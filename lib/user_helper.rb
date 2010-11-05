354ace2b1d55d8241cf5231d2f7c4f6ea03d3968
GilbertLiddell <gilbertliddell@hotmail.com>
2010-11-05 11:11:42 +0000

    added restricted confirmation link to on user helper

--- lib/user_helper.rb
+++ lib/user_helper.rb
@@ -5,5 +5,10 @@ module Users
     def restricted_link_to(name, url)
       link_to name, url if current_user.can_access(url)
     end
+
+    def restricted_confirmation_link_to(name, url, message, method)
+      url = url if current_user.can_access(url)
+      link_to name, url, :confirm => message, :method => method
+    end
   end
 end
