196456d366eb4d88184b407f2889aec75ce2ed09
Chris Wright <cjwright83y@gmail.com>
2012-10-29 10:24:25 +0000

    only include migrations and stampable modules if ActiveRecord is defined

--- lib/users_cas_auth.rb
+++ lib/users_cas_auth.rb
@@ -2,18 +2,22 @@ $: << File.expand_path(File.dirname(__FILE__)) + '/users_cas_auth'
 
 if defined?(Rails) && Rails::VERSION::MAJOR == 3
   require 'engine'
-  require 'migration_helper'
-  require 'stampable'
   require 'user_session'
   require 'user_helper'
   require 'user_test_helper'
   require 'users_controller'
+  if defined?(ActiveRecord)
+    require 'migration_helper'
+    require 'stampable'
+  end
 else
-  require 'migration_helper'
-  require 'stampable'
   require 'user'
   require 'user_session'
   require 'user_helper'
   require 'user_test_helper'
   require 'users_controller'
+  if defined?(ActiveRecord)
+    require 'migration_helper'
+    require 'stampable'
+  end
 end
