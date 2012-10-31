7b25f8dddde29751221fa7b33797e93902d4dd91
Chris Wright <cjwright83y@gmail.com>
2012-10-31 15:14:54 +0000

    ensured that new route for logging out is compatible with rails 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
-Rails.application.routes.draw do |map|
-  match "/pages/logout" => "users#logout"
+if defined?(Rails) && Rails::VERSION::MAJOR == 3
+  Rails.application.routes.draw do |map|
+    match "/pages/logout" => "application#logout"
+  end
+else
+  # Redefine clear! method to do nothing (usually it erases the routes)
+  class << ActionController::Routing::Routes;self;end.class_eval do
+    define_method :clear!, lambda {}
+  end
+
+  ActionController::Routing::Routes.draw do |map|
+    map.connect '/pages/logout', :controller => 'application', :action => 'logout'
+  end
 end
